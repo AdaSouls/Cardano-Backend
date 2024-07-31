ceb256c0a774aca9a9b1eb3674383a348c4c86c9
Falcon <matias.falcone@gmail.com>
2024-07-31 18:02:52 +1000

    Delete utils directory

--- utils/prod/heroku_create_app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-. "$CWD/vars.sh"
-
-heroku create --remote=$GIT_REMOTE --buildpack=heroku/nodejs $HEROKU_APP_NAME
-heroku git:remote --remote=$GIT_REMOTE --app=$HEROKU_APP_NAME
-
-. "$CWD/heroku_set_all_config_vars.sh"
--- utils/prod/vars.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-CWD="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-NODE_ENV=production
-HEROKU_APP_NAME=adasouls-prod-backend
-GIT_REMOTE=h-prod
